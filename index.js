dfe8720ba379be381ba7a6c514ac6ce88aab529a
Sumonsnote <sohankhance@gmail.com>
2022-05-23 00:19:06 +0600

    server setup and get api

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.txk2a.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('auto-car-inc').collection('parts')
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query)
+            const parts = await cursor.toArray()
+            res.send(parts)
+        })
+
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const parts = await partsCollection.findOne(query);
+            res.send(parts);
+        })
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello From Auto Car Inc')
+})
+
+app.listen(port, () => {
+    console.log(`Auto Car Inc App listening on port ${port}`)
+})
